9d1d32673baba5a4d54194967176e0eda93ad281
danna202 <105758399+danna202@users.noreply.github.com>
2023-07-10 20:47:02 -0400

    Update mecurySedan.js

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,9 +1,3 @@
-//this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
-
-//this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
 const VehicleModule = require('./vehicle').Vehicle
 
 class Car extends VehicleModule {
